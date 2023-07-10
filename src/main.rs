ff734bf437244fb32db8dd0ebd8f0b2debdc5ed5
joakim <joakimt04@gmail.com>
2023-07-10 21:19:04 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Startup, spawn_player)
+        .run();
+}
+
+#[derive(Component)]
+
+pub struct Player {}
+
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("explosionsheet.png"),
+            ..default()
+        },
+        Player {},
+    ));
+}
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
 }
